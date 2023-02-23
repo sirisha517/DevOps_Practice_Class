9789bc7c77e0addefb540bb68f003392c168ecc6
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-23 20:07:41 +0530

    updating roboshop_shell

--- redis.sh
+++ redis.sh
@@ -1,25 +1,26 @@
 source common.sh
 
-print_head "Installing redis repo files"
+print_head "Setup Redis Repo"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
-status_check $?
+status_check
 
-print_head "enable dnf module"
+print_head "Enable Redis 6.2 dnf Module"
 dnf module enable redis:remi-6.2 -y &>>${log_file}
-status_check $?
+status_check
 
- print_head "Installing Redis"
+print_head "Install Redis"
 yum install redis -y  &>>${log_file}
-status_check $?
+status_check
 
-print_head "update Redis listen redis"
+print_head "Update Redis Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
-status_check $?
+status_check
 
-print_head "enable Redis"
-systemctl enable redis
-status_check $?
 
-print_head "start Redis"
-systemctl restart redis
-status_check $?
+print_head "Enable Redis"
+systemctl enable redis &>>${log_file}
+status_check
+
+print_head "Start Redis"
+systemctl restart redis &>>${log_file}
+status_check
