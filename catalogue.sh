4679640d083a7b1c27c1cd128e277485e1949a25
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-23 14:06:45 +0530

    updating roboshop_shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,47 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "congigure nodejs repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.devops517test.online </app/schema/catalogue.js
+print_head "Install nodejs repo"
+yum install nodejs -y &>>${log_file}
+
+print_head "roboshop user added"
+useradd roboshop &>>${log_file}
+
+print_head "create application directory"
+mkdir /app &>>${log_file}
+
+print_head "remove old content"
+rm -rf /app/* &>>${log_file}
+
+print_head "download app content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+cd /app
+
+print_head "extracting app content"
+unzip /tmp/catalogue.zip &>>${log_file}
+
+print_head "Installing nodejs dependencies"
+npm install &>>${log_file}
+
+print_head "copy systemd service file"
+cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload systemd"
+systemctl daemon-reload &>>${log_file}
+
+print_head "enable catalogue service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "start catalogue service"
+systemctl start catalogue &>>${log_file}
+
+print_head "copy mongodb repo file"
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+print_head "Install mongo client"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "load schema"
+mongo --host mongodb.devops517test.online </app/schema/catalogue.js &>>${log_file}
