fd89fae66b382f682b9e7e14fcfca0483a57ff62
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-23 16:39:40 +0530

    updating roboshop_shell

--- common.sh
+++ common.sh
@@ -5,3 +5,11 @@ rm -f ${log_file}
 print_head() {
  echo -e "\e[35m$1\e[0m" # if we change colour in one place it will effect in all places
   }
+
+status_check(){
+  if [ $? -eq 0 ]; then
+    echo SUCESS
+    else
+      echo FAILURE
+  fi
+}
--- frontend.sh
+++ frontend.sh
@@ -3,34 +3,30 @@ source common.sh
 
 print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCESS
-  else
-    echo FAILURE
-fi
+status_check $?
 
 print_head "Removing  old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo $?
+status_check $?
 
 
 print_head "Downloading frontend"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Extracting download frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Copying Nginx configs for RoboShop"
 cp ${code_dir}/configs/nginx.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo $?
+status_check $?
 
 print_head "enable Nginx"
 systemctl enable nginx &>>${log_file}
-echo $?
+status_check $?
 
 print_head "start Nginx"
 systemctl restart nginx &>>${log_file}
-echo $?
+status_check $?
