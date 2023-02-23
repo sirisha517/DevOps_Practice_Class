d3e575b937256e7ec41f7cc364a554cc381de9b0
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-23 22:28:45 +0530

    updating roboshop_shell

--- rabbitMq.sh
+++ rabbitMq.sh
@@ -27,7 +27,10 @@ systemctl start rabbitmq-server &>>${log_file}
 status_check $?
 
 print_head "add application user"
+rabbitmqctl list_users | grep roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
 rabbitmqctl add_user roboshop ${roboshop_app_password} &>>${log_file}
+fi
 status_check $?
 
 print_head "configure permission for app user"
