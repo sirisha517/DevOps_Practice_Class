7cd18ee82620a35ce869c611baf8b1acd76309dd
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-23 19:36:27 +0530

    updating roboshop_shell

--- common.sh
+++ common.sh
@@ -15,3 +15,71 @@ status_check(){
       exit 1
   fi
 }
+
+NODEJS(){
+  print_head "configure nodejs repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Install nodejs repo"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  print_head "roboshop user added"
+  id roboshop &>>${log_file}
+  if [ $? -ne 0 ]; then # user doesnot exists then create
+    useradd roboshop &>>${log_file}
+    fi
+  status_check $?
+
+  print_head "create application directory"
+  if [ ! -d /app ] ; then # if not exists then create directory
+    mkdir /app &>>${log_file}
+    fi
+  status_check $?
+
+  print_head "remove old content"
+  rm -rf /app/* &>>${log_file}
+  status_check $?
+
+  print_head "download app content"
+  curl -L -o /tmp/${component}.service https://roboshop-artifacts.s3.amazonaws.com/${component}.service &>>${log_file}
+  status_check $?
+  cd /app
+
+  print_head "extracting app content"
+  unzip /tmp/${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "Installing nodejs dependencies"
+  npm install &>>${log_file}
+  status_check $?
+
+  print_head "copy systemd service file"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "Reload systemd"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+
+  print_head "enable ${component}.service"
+  systemctl enable ${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "start ${component}.service"
+  systemctl start ${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "copy mongodb repo file"
+  cp  ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+  status_check $?
+
+  print_head "Install mongo client"
+  yum install mongodb-org-shell -y &>>${log_file}
+  status_check $?
+
+  print_head "load schema"
+  mongo --host mongodb.devops517test.online </app/schema/${component}.service &>>${log_file}
+  status_check $?
+}
